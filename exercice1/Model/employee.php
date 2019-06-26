24c749ebd31d7d4411ce8ca633b285ab6aacf151
Martinq76 <martin.queguiner.42@gmail.com>
2019-06-26 10:57:01 +0200

    final commit

---
+++ exercice1/Model/employee.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * 
+ */
+class employee {
+
+    /**
+     * 
+     */
+    private $lastName;
+
+    /**
+     * 
+     */
+    private $firstName;
+
+    /**
+     * 
+     */
+    private $salary;
+
+    /**
+     * Default constructor
+     */
+    public function __construct() {
+    }
+
+    /**
+     * 
+     */
+    public function work() {
+        // TODO implement here
+    }
+
+}
---
+++ exercice1/Model/executive.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * 
+ */
+class executive extends employee {
+
+    /**
+     * 
+     */
+    private $bonus;
+
+    /**
+     * Default constructor
+     */
+    public function __construct() {
+    }
+
+    /**
+     * 
+     */
+    public function to have responsibilities() {
+        // TODO implement here
+    }
+
+}
